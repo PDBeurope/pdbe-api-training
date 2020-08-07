0b5bfa85267c8783ff2f01c46d1ba49c530c3805
jmb <jmb@ebi.ac.uk>
2020-08-07 11:17:26 +0100

    updated sequence search sorting

--- python_modules/api_modules.py
+++ python_modules/api_modules.py
@@ -1,22 +1,17 @@
-import requests
 import pandas as pd
+import requests
 
 # settings for PDBe API
 base_url = "https://www.ebi.ac.uk/pdbe/"  # the beginning of the URL for PDBe's API.
-
-# api_base = base_url + "api/"
-
 search_url = base_url + 'search/pdb/select?'  # the rest of the URL used for PDBe's search API.
 
 
 def make_request(search_term, number_of_rows=10):
     """
-    This function can make GET requests to
-    the PDBe search API
-
-    :param url: String,
-    :param pdb_id: String
-    :return: JSON
+    makes a get request to the PDBe API
+    :param search_term: the terms used to search
+    :param number_of_rows: number or rows to return - limited to 10
+    :return dict: response JSON
     """
     search_variables = '&wt=json&rows={}'.format(number_of_rows)
     url = search_url + search_term + search_variables
@@ -32,45 +27,127 @@ def make_request(search_term, number_of_rows=10):
 
 
 def format_search_terms(search_terms, filter_terms=None):
-    # print('formatting search terms: %s' % search_terms)
-    search_string = ''
+    """
+    Change search terms from a dictionary to a string for the URL
+    :param dict search_terms: dictionary of search terms
+    :param lst filter_terms: list of terms to filter with
+    :return str: search string
+    """
     filter_string = ''
     search_list = []
     if isinstance(search_terms, dict):
         for key in search_terms:
             term = search_terms.get(key)
-            if ' ' in term:
-                if not '"' in term:
+            if ' ' in term and not '[' in term:
+                if '"' not in term:
                     term = '"{}"'.format(term)
-                elif not "'" in term:
+                elif "'" not in term:
                     term = "'{}'".format(term)
             search_list.append('{}:{}'.format(key, term))
         search_string = ' AND '.join(search_list)
+        if filter_terms:
+            filter_string = '&fl={}'.format(','.join(filter_terms))
+        final_search_string = 'q={}{}'.format(search_string, filter_string)
+        return final_search_string
     else:
-        if '&' in search_terms:
-            search_string = search_terms.replace('&', ' AND ')
-        else:
-            search_string = search_terms
+        print('search terms is not defined as a dictionary')
+        return ''
+
+
+def format_sequence_search_terms(sequence, group_field='pdb_id', filter_terms=None):
+    """
+    Format parameters for a sequence search
+    :param str sequence: one letter sequence
+    :param str group_field: Field to group by
+    :param lst filter_terms: Terms to filter the results by
+    :return str: search string
+    """
+    params = {'group': 'true',
+              'group.field': group_field,
+              'group.ngroups': 'true',
+              'json.nl': 'map',
+              'start': '0',
+              'sort': 'fasta(e_value) asc',
+              'xjoin_fasta': 'true',
+              'bf': 'fasta(percentIdentity)',
+              'xjoin_fasta.external.expupperlim': '0.1',
+              'xjoin_fasta.external.sequence': sequence,
+              'q': '*:*',
+              'fq': '{!xjoin}xjoin_fasta'
+              }
+    search_list = []
+    for item in params:
+        value = params[item]
+        search_list.append('{}={}'.format(item, value))
+    search_string = '&'.join(search_list)
     if filter_terms:
         filter_string = '&fl={}'.format(','.join(filter_terms))
-    # print('formatted search terms: %s' % search_string)
-    final_search_string = 'q={}{}'.format(search_string, filter_string)
-    return final_search_string
+        search_string += filter_string
+
+    return search_string
 
 
-def run_search(search_terms, filter_terms=None, number_of_rows=100):
-    search_term = format_search_terms(search_terms, filter_terms)
-
+def run_sequence_search(sequence, filter_terms=None, number_of_rows=10):
+    """
+    Runs a sequence search and results the results
+    :param str sequence: sequence in one letter code
+    :param lst filter_terms: terms to filter the results by
+    :param int number_of_rows: number of results to return
+    :return lst: List of results
+    """
+    group_field = 'pdb_id'
+    search_term = format_sequence_search_terms(sequence=sequence, filter_terms=filter_terms, group_field=group_field)
     response = make_request(search_term, number_of_rows)
-    results = response.get('response', {}).get('docs', [])
-    print('Number of results for {}: {}'.format(','.join(search_terms.values()), len(results)))
-    return results
+    results = response.get('grouped', {}).get(group_field, {}).get('groups', [])
+    print('Number of results {}'.format(len(results)))
+
+    raw_fasta_results = response.get('xjoin_fasta').get('external')
+    fasta_results = {}
+
+    for fasta_row in raw_fasta_results:
+        join_id = fasta_row.get('joinId')
+        fasta_doc = fasta_row.get('doc', {})
+        percent_identity = fasta_doc.get('percent_identity')
+        e_value = fasta_doc.get('e_value')
+        fasta_results[join_id] = {'e_value': e_value,
+                                  'percentage_identity': percent_identity}
+
+    ret = []
+    for row in results:
+        doc = row.get('doclist', {}).get('docs', [])[0]
+        group_id = doc.get(group_field)
+        group_fasta_results = fasta_results.get(group_id, {})
+        doc['e_value'] = group_fasta_results.get('e_value')
+        doc['percentage_identity'] = group_fasta_results.get('percentage_identity')
+
+        ret.append(doc)
+    return ret
+
+
+def run_search(search_terms, filter_terms=None, number_of_rows=10):
+    """
+    Run the search with set of search terms
+    :param dict search_terms: dictionary of search terms
+    :param list filter_terms: list of terms to filter by
+    :param int number_of_rows: number of search rows to return
+    :return lst: list of results
+    """
+    search_term = format_search_terms(search_terms, filter_terms)
+    if search_term:
+        response = make_request(search_term, number_of_rows)
+        if response:
+            results = response.get('response', {}).get('docs', [])
+            print('Number of results for {}: {}'.format(','.join(search_terms.values()), len(results)))
+            return results
+    print('No results')
+    return []
 
 
 def change_lists_to_strings(results):
     """
-    input - list of results from search
-    output - list of results with lists changed into strings
+    updates lists to strings for loading into Pandas
+    :param dict results: dictionary of results to process
+    :return dict: dictionary of results
     """
     for row in results:
         for data in row:
@@ -82,9 +159,10 @@ def change_lists_to_strings(results):
 
     return results
 
+
 def pandas_dataset(list_of_results):
-    results = change_lists_to_strings(list_of_results) # we have added our function to change lists to strings
-    df = pd.DataFrame(list_of_results)
+    results = change_lists_to_strings(list_of_results)  # we have added our function to change lists to strings
+    df = pd.DataFrame(results)
 
     return df
 
@@ -114,14 +192,13 @@ def pandas_plot_multi_groupby(results, first_column_to_group_by, second_column_t
     df = pandas_dataset(results)
     new_df = df.groupby([first_column_to_group_by, second_column_to_group_by])
     ds = new_df.count().unstack().reset_index(first_column_to_group_by)
-    ds.plot(x=first_column_to_group_by, y=y_axis, kind=graph_type)
+    ds.plot(x=first_column_to_group_by, y=y_axis, kind=graph_type).legend(bbox_to_anchor=(1.04, 1))
 
 
 def pandas_plot_multi_groupby_min(results, first_column_to_group_by, second_column_to_group_by, graph_type='line',
                                   use_min=False, use_max=False):
     df = pandas_dataset(results)
     new_df = df.groupby([first_column_to_group_by])[second_column_to_group_by]
-    ds = None
     if use_min:
         ds = new_df.min()
     elif use_max:
